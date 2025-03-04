b1a326e3c93b212d251170a0bb97373ddf04e8b0
dom.d. <dominic.destrait@neuefische.de>
2025-03-04 10:44:30 +0100

    add Main Class

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name1 = "John Doe";
+        String name2 = "Jane Doe";
+
+        greetStudent(name1);
+        greetStudent(name2);
+
+        System.out.println(isEven(20));
+
+        System.out.println(multiply(5, 2));
+
+    }
+
+    // scope (static) RückgabeTyp name (ÜbergabeParameter) {}
+    public static void greetStudent(String name) {
+
+        System.out.println("Hallo, " + name + " wie geht es dir?");
+        System.out.println("Willkommen im Java Bootcamp");
+    }
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
